d1e08b07848b9ed2162b9810c31ec9772cafd8e4
xp <wxp.cn@live.com>
2023-02-05 13:30:16 +0800

    add file

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
-package main // 声明 main 包，表明当前是一个可执行程序
+package main
 
-import "fmt" // 导入内置 fmt
+import (
+	"fmt"
 
-func main() { // main函数，是程序执行的入口
-	fmt.Println("Hello World!") // 在终端打印 Hello World!
+	"github.com/blogxp/goapi/configs"
+	"github.com/blogxp/goapi/pkg/env"
+	"github.com/blogxp/goapi/pkg/logger"
+	"github.com/blogxp/goapi/pkg/timeutil"
+	"go.uber.org/zap"
+)
+
+func main() {
+
+	fmt.Println("Hello World!")
+	test := configs.Get()
+	fmt.Println(test.Mail.Host)
+	// 初始化 access logger
+	accessLogger, err := logger.NewJSONLogger(
+		logger.WithDisableConsole(),
+		logger.WithField("domain", fmt.Sprintf("%s[%s]", configs.ProjectName, env.Active().Value())),
+		logger.WithTimeLayout(timeutil.CSTLayout),
+		logger.WithFileP(configs.ProjectAccessLogFile),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// 初始化 cron logger
+	cronLogger, err := logger.NewJSONLogger(
+		logger.WithDisableConsole(),
+		logger.WithField("domain", fmt.Sprintf("%s[%s]", configs.ProjectName, env.Active().Value())),
+		logger.WithTimeLayout(timeutil.CSTLayout),
+		logger.WithFileP(configs.ProjectCronLogFile),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		_ = accessLogger.Sync()
+		_ = cronLogger.Sync()
+	}()
+	accessLogger.Fatal("http server startup err", zap.Error(err))
 }
